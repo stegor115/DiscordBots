5b9f8e2b352918db533b1d943c9af70aa6876e12
Stephen Gordon <stegor115@gmail.com>
2018-09-18 04:17:22 -0400

    Initial commit

---
+++ ErwinBot/ErwinBot/MyBot.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErwinBot
+{
+    class MyBot
+    {
+        public MyBot()
+        {
+            DiscordClient discord;
+        }
+    }
+}
