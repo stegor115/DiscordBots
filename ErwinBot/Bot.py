f9327bb4544cab521ad04d0999a3ca178b108af4
Stephen Gordon <stegor115@gmail.com>
2018-09-18 15:48:44 -0400

    Functions properly, needs some tweaking

--- ErwinBot/Bot.py
+++ ErwinBot/Bot.py
@@ -6,22 +6,60 @@ TOKEN = file.read()
 
 client = discord.Client()
 
+boolQuestionAsked = False #Default value
+
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        print('Message sent.')
-        await client.send_message(message.channel, msg)
+    #if message.content.startswith('!hello'):
+    #    msg = 'Hello {0.author.mention}'.format(message)
+    #    await client.send_message(message.channel, msg)
+    #    print('Message sent.')
+
+    if message.content.startswith('!image'): #Debug only, delete later
+        image = "img/sorry.png"
+        await client.send_file(message.channel, image)
+        print("Debug Image sent.")
+
+    inMessage = message.content
+    global boolQuestionAsked
+    #Checks if the message is a question, and if a question has already been asked.
+    if inMessage[len(inMessage)-1]== '?' and boolQuestionAsked == False:
+        print ("Question recognized, awaiting response.")
+        boolQuestionAsked = True
+
+    #Checks if next message is an acceptable reply, only if a question was asked first.
+    #TO-DO, Make this only happen if the next message has a different author.
+    elif acceptableWhat(message.content) and boolQuestionAsked == True:
+        image = "img/sorry.png"
+        await client.send_file(message.channel, image)
+        print("Strange thing to ask detected. Image sent.")
+        boolQuestionAsked = False
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
+    print('------------------------------------')
+    print(client.user.name + " Online!")
     print(client.user.id)
-    print('------')
+    print('------------------------------------')
+
+def acceptableWhat(inMessage):
+    #Disgusting list of if else spam I hate it
+    inMessage = inMessage.lower()
+    if inMessage == "what?":
+        return True
+    elif inMessage == "what?":
+        return True
+    elif inMessage == "wat?":
+        return True
+    elif inMessage == "wat":
+        return True
+    elif inMessage == "?":
+        return True
+    else:
+        return False
 
 client.run(TOKEN)
