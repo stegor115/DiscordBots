a3dc85df404992ec3c45e0345d0316e9c888db73
Stephen Gordon <stegor115@gmail.com>
2018-09-18 15:05:26 -0400

    Hello World done

---
+++ ErwinBot/Bot.py
@@ -0,0 +1,27 @@
+import discord
+
+#Due to this bot being available on Github, for security the token is parsed in from an excluded file.
+file = open('token.txt','r')
+TOKEN = file.read()
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        print('Message sent.')
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
