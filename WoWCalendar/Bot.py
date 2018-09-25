891edbe3efa07013de76ef1d9e161e374b600a2e
Stephen Gordon <stegor115@gmail.com>
2018-09-25 19:53:22 -0400

    Authorized user command work done

--- WoWCalendar/Bot.py
+++ WoWCalendar/Bot.py
@@ -10,13 +10,8 @@ TOKEN = file.read()
 
 client = discord.Client()
 
-#Key = Server ID, Data = List of Event objects
-mapServerEvents = dict()
-#Key = Server ID, Data = List of Authorized Users
-mapServerAuthUsers = dict()
-
-#Iterate through ALL of data/events directory and load into mapServer
-#Do the same for authorized user lists in data/users
+#Iterate through ALL of events directory and load into mapServer
+#Do the same for authorized user lists
 
 @client.event
 async def on_message(message):
@@ -25,14 +20,12 @@ async def on_message(message):
         return
 
     inMessage = message.content
-    #Globals
-    global mapServerEvents
+    serverID = message.server
 
     # Creation of an event
     # !create <name> <date> <time> <description>
     # eventID is created via count of readLines in server's file
     if inMessage.startswith("!create "):
-        await client.send_message(message.channel, "Hello World!") #DEBUG
         parameters = inMessage.split()
         print(parameters) #DEBUG
         if len(parameters) == 5:
@@ -42,18 +35,34 @@ async def on_message(message):
         return #Stop rest of script from running
     #Usage explanation
     elif inMessage == "!create":
-        await client.send_message(message.channel, "Usage:\n!create <name> <date> <server time> <description>\
+        await client.send_message(message.channel, "__**Usage:**__\n!create <name> <date> <server time> <description>\
         \nName example: Awesome-Guild-Meeting\nDate example: 1-1-2000")
         return #Stop rest of script from running
 
     #Authorize members of non-automated ranks
     #Parameters = !authorize <@User>
     if inMessage.startswith("!authorize "):
+        parameters = inMessage.split()
+        if len(parameters) == 2:
+            print(*message.mentions)
+            serverID = 'data/users' + str(serverID) + ".txt"
+            myWriteFile = open(serverID, "w+")
+            myReadFile = open(serverID, "r")
+            for line in myReadFile:
+                if line in message.mentions:
+                    await client.send_message(message.channel, str(line) + " is already an authorized user.")
+                    message.mentions.remove(line)
+            #End for
+            for i in range(len(message.mentions)):
+                myWriteFile.write(str(message.mentions[i]))
+                await client.send_message(message.channel, "Added " + message.mentions[i] + " as an authorized user.")
+        else:
+            await client.send_message(message.channel, "Invalid entry, type \"!authorize\" for help.")
         #Authorize users here
         return #Debug
     elif inMessage == "!authorize":
         #Usage explaination
-        return
+        await client.send_message(message.channel, "__**Usage:**__\n!authorize <@user>")
 
 @client.event
 async def on_ready():
